3b6e56e8d503da42fdb9d6caa62d3822b70446d9
dokinho <doks7081@live.com>
2024-09-28 09:05:05 +0200

    Fix display bug by removing dynamic tailwind class names

--- client/src/components/chat_elements/ChatMessage.tsx
+++ client/src/components/chat_elements/ChatMessage.tsx
@@ -1,19 +1,30 @@
 import { ReactNode } from "react";
 
 const ChatMessage = ({you=true, children}: { you?: boolean, children: ReactNode}) => {
-  const color = you ? "queen-blue" : "medium-ruby";
-  const reverseClass = you ? "flex-row-reverse" : "";
+  const styles = you
+  ? {
+    reverseClass: "flex-row-reverse",
+    borderColor: "border-queen-blue",
+    backgroundColor: "bg-queen-blue"
+  }
+  : {
+    reverseClass: "",
+    borderColor: "border-medium-ruby",
+    backgroundColor: "bg-medium-ruby"
+  }
 
   return (
-    <div className={`chat-message flex ${reverseClass} gap-2 items-end mb-2 last:mb-0`}>
+    <div className={`chat-message flex ${styles.reverseClass} gap-2 items-end mb-2 last:mb-0`}>
       <img
         src="/src/assets/images/avatar-ph.png"
         alt="User profile picture"
         width={36}
         height={36}
-        className={`border border-${color} rounded-full`}
+        className={`border ${styles.borderColor} rounded-full`}
       />
-      <p className={`p-2 bg-${color} rounded-xl shadow-sm max-w-[80%]`}>{children}</p>
+      <p className={`p-2 ${styles.backgroundColor} rounded-xl shadow-sm max-w-[80%]`}>
+        {children}
+      </p>
     </div>
   );
 }
