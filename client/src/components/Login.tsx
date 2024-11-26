5cff4e715e43203f4371fc5f689b847820f6f1e4
dokinho <doks7081@live.com>
2024-11-26 19:15:55 +0100

    Turn off borwser autocomplete on login forms

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -9,7 +9,8 @@ const Login = ({ nickname, setNickname, login }: LoginPropTypes) => {
 
   return (
     <div id="login-form-container" className="flex justify-center items-center w-full h-full">
-      <form id="login-form" className="flex flex-col w-80 justify-center border-2 border-black p-8 bg-queen-blue rounded-lg shadow-lg">
+      <form id="login-form" className="flex flex-col w-80 justify-center border-2 border-black p-8 bg-queen-blue rounded-lg shadow-lg"
+        autoComplete="off">
         <label htmlFor="nickname" className="text-xl text-black font-semibold">Nickname</label>
         <input id="nickname" type="text" value={nickname} onChange={handleChange} className="font-xl p-3 mt-2 rounded-lg bg-bubbles" />
         <button className="border border-black rounded-md p-2 mt-8 font-bold bg-medium-ruby" onClick={login}>Login</button>
